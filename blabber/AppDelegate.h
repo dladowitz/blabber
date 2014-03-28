249be49bf57988657a0ac5a610d9074524b204dc
David Ladowitz <david@ladowitz.com>
2014-03-27 20:29:02 -0700

    Initial Commit

---
+++ blabber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blabber
+//
+//  Created by David Ladowitz on 3/27/14.
+//  Copyright (c) 2014 Little Cat Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blabber/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  blabber
+//
+//  Created by David Ladowitz on 3/27/14.
+//  Copyright (c) 2014 Little Cat Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
