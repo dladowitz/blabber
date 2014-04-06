76cadbdf844722a8b2ff2acd921d8681fa0c3b08
David Ladowitz <david@ladowitz.com>
2014-04-05 22:18:56 -0700

    adding menu view controller

---
+++ blabber/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  blabber
+//
+//  Created by David Ladowitz on 4/4/14.
+//  Copyright (c) 2014 Little Cat Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
